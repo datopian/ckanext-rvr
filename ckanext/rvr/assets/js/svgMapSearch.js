24e1bc71d5b4b2980881686ca0871bebf4428357
gavram <dragan.avramovic@datopian.com>
2022-12-04 16:02:38 +0100

    [feature] SVG map search

---
+++ ckanext/rvr/assets/js/svgMapSearch.js
@@ -0,0 +1,13 @@
+function maximizeMap() {
+    var mapMaximized = document.getElementById("maximized");
+    var mapMinimized = document.getElementById("minimized");
+    mapMaximized.style.display = "block";
+    mapMinimized.style.display = "none";
+}
+
+function minimizeMap() {
+    var mapMaximized = document.getElementById("maximized");
+    var mapMinimized = document.getElementById("minimized");
+    mapMaximized.style.display = "none";
+    mapMinimized.style.display = "block";
+}
