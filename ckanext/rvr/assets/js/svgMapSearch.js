062f456b0827309a133f04ce65bd5fbde32869f2
gavram <dragan.avramovic@datopian.com>
2023-03-29 08:16:22 +0200

    [xl] Groups mandatory field

--- ckanext/rvr/assets/js/svgMapSearch.js
+++ ckanext/rvr/assets/js/svgMapSearch.js
@@ -1,3 +1,4 @@
+// SVG Map Search helpers
 function maximizeMap() {
     var mapMaximized = document.getElementById("maximized");
     var mapMinimized = document.getElementById("minimized");
@@ -12,6 +13,7 @@ function minimizeMap() {
     mapMinimized.style.display = "block";
 }
 
+// Mandatory fields helpers
 function getSelectedOptions(sel) {
     var opts = [],
       opt;
@@ -32,6 +34,115 @@ function getSelectedOptions(sel) {
     return group_string;
 }
 
+function checkTitle() {
+  if (document.getElementById("field-title").value == "") {
+      return false;
+  }
+}
+
+function checkGroups() {
+  if (document.getElementById("field-groups__1__id").value == "") {
+    return false;
+  }
+}
+
+function checkOrg() {
+  if (document.getElementById("field-organizations").value == "") {
+    return false;
+  }
+}
+
+function checkNotes() {
+  if (document.getElementById("field-notes").value == "") {
+    return false;
+  }
+}
+
+function checkAll() {
+  if (checkTitle() == false || checkGroups() == false || checkOrg() == false || checkNotes() == false) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function setButtonTooltip() {
+  var error_title = "";
+  var error_notes = "";
+  var error_groups = "";
+  var error_org = "";
+
+  if (checkTitle() == false) {
+    error_title =  "\tName: Fehlender Wert\n";
+  }
+  if (checkNotes() == false) {
+    error_notes =  "\tBeschreibung: Fehlender Wert\n";
+  }
+  if (checkGroups() == false) {
+    error_groups =  "\tGruppen: Fehlender Wert\n";
+  }
+  if (checkOrg() == false) {
+    error_org =  "\tOrganisation: Fehlender Wert\n";
+  }
+
+  var error_message = "Das Formular enthält unzulässige Einträge:\n" + error_title + error_notes + error_org + error_groups;
+
+  return error_message;
+}
+
+function groupChange() {
+  if (checkAll() == false) {
+    // Set the button class="btn btn-primary" in div class="form-actions" to disabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = true;
+    // Set the tooltip of the button class="btn btn-primary" in div class="form-actions" to the error message
+    document.getElementsByClassName("btn btn-primary")[0].title = setButtonTooltip();
+  } else {
+    // Set the button class="btn btn-primary" in div class="form-actions" to enabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = false;
+    document.getElementsByClassName("btn btn-primary")[0].title = "";
+  }
+}
+
+function orgChange() {
+  if (checkAll() == false) {
+    // Set the button class="btn btn-primary" in div class="form-actions" to disabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = true;
+    // Set the tooltip of the button class="btn btn-primary" in div class="form-actions" to the error message
+    document.getElementsByClassName("btn btn-primary")[0].title = setButtonTooltip();
+  } else {
+    // Set the button class="btn btn-primary" in div class="form-actions" to enabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = false;
+    document.getElementsByClassName("btn btn-primary")[0].title = "";
+  }
+}
+
+document.getElementById("field-notes").addEventListener("change", function(event) {
+  if (checkAll() == false) {
+    // Set the button class="btn btn-primary" in div class="form-actions" to disabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = true;
+    // Set the tooltip of the button class="btn btn-primary" in div class="form-actions" to the error message
+    document.getElementsByClassName("btn btn-primary")[0].title = setButtonTooltip();
+  } else {
+    // Set the button class="btn btn-primary" in div class="form-actions" to enabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = false;
+    document.getElementsByClassName("btn btn-primary")[0].title = "";
+  }
+});
+
+document.getElementById("field-title").addEventListener("change", function(event) {
+  if (checkAll() == false) {
+    // Set the button class="btn btn-primary" in div class="form-actions" to disabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = true;
+    // Set the tooltip of the button class="btn btn-primary" in div class="form-actions" to the error message
+    document.getElementsByClassName("btn btn-primary")[0].title = setButtonTooltip();
+  } else {
+    // Set the button class="btn btn-primary" in div class="form-actions" to enabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = false;
+    document.getElementsByClassName("btn btn-primary")[0].title = "";
+  }
+});
+
+// On submit form event listener
 document.getElementById("dataset-edit").addEventListener("submit", function(event) {
     // Get selected options from the element with id="field-groups__0__id"
     var selectedOptions = getSelectedOptions(document.getElementById("field-groups__1__id"));
@@ -44,5 +155,16 @@ document.getElementById("dataset-edit").addEventListener("submit", function(even
     }
     // Set the value of the field with name="group_string" to the selectedOptions array
     document.getElementsByName("group_string")[0].value = selectedOptions;
-    
 });
+
+// On body change event listener
+document.addEventListener("DOMContentLoaded", function(event) {
+  if ( checkAll() == false) {
+    // Set the button class="btn btn-primary" in div class="form-actions" to enabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = true;
+  } else {
+    // Set the button class="btn btn-primary" in div class="form-actions" to enabled
+    document.getElementsByClassName("btn btn-primary")[0].disabled = false;
+  }
+});
+
