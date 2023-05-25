692acdd598b9cb3c305d66f44cf0bfd3ca9ba29b
sagargg <info@sagarg.com.np>
2023-05-25 12:00:55 +0545

    [fix][xs]: map clear link not clickable bug fix

--- ckanext/rvr/assets/js/collapseArrow.js
+++ ckanext/rvr/assets/js/collapseArrow.js
@@ -7,10 +7,20 @@ this.ckan.module('collapse-arrow', function($, _) {
       setupCollapseToggle: function() {
         var toggleElements = this.el
 
-        if (toggleElements.length === 0) {
-          return;
-        }
-        toggleElements.on('click', function() {
+        toggleElements.on('click', function(event) {
+          if(toggleElements.attr('aria-controls') === 'collapse-dataset-map'){
+            
+            if ($('body').hasClass('dataset-map-expanded')) {
+              $('body').removeClass('dataset-map-expanded');
+            }
+            // Prevents the collapse 
+            toggleElements.removeAttr('data-toggle');
+            if (event.target.className === 'action') {
+              // allow clear link to click through
+              return;
+            }
+            toggleElements.attr('data-toggle', 'collapse');
+          }
           var spanElement = $(this).find('span');  
           spanElement.toggleClass('collapse-arrow collapsed-arrow');
         });
