258f5e8fdb26592f9f125d87c04e8c2e40707c2d
sagargg <info@sagarg.com.np>
2023-05-25 08:07:22 +0545

    [improve][xs]: repositioning faq nav in header

--- ckanext/rvr/views/__init__.py
+++ ckanext/rvr/views/__init__.py
@@ -1,5 +1,5 @@
 from ckanext.rvr.views.dataset import dataset_blueprint
-from ckanext.rvr.views.faq import faq_blueprint
+from ckanext.rvr.views.faq import page_blueprint
 
 def get_rvr_blueprint():
-    return [dataset_blueprint, faq_blueprint]
+    return [dataset_blueprint, page_blueprint]
--- ckanext/rvr/views/faq.py
+++ ckanext/rvr/views/faq.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 import ckan.plugins.toolkit as toolkit
 
-faq_blueprint = Blueprint('faq', __name__)
+page_blueprint = Blueprint('page', __name__)
 
 def faq_controller():
     return toolkit.render('home/faq.html')
 
-faq_blueprint.add_url_rule('/faq', 'faq', faq_controller)
+page_blueprint.add_url_rule('/faq', 'faq', view_func=faq_controller)
