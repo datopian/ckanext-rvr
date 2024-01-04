96bf5cc95468eae1f54bec5b93ed123a344f725b
Meiran Zhiyenbayev <meiran1991@gmail.com>
2024-01-04 15:21:11 +0600

    Update rvrBBOXGenerator.js zoom in

--- ckanext/rvr/assets/js/rvrBBOXGenerator.js
+++ ckanext/rvr/assets/js/rvrBBOXGenerator.js
@@ -70,7 +70,7 @@ this.ckan.module('rvr-bbox-generator', function ($, _) {
                 drawControlTooltips: true,
                 maxBounds: module.options.default_extent,
                 maxBoundsViscosity: 1,
-                minZoom: 7.5
+                minZoom: 8
             }
             map = ckan.rvrWebMap(
                 'dataset-map-container',
