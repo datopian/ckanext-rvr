c0752e194b585f13a5baf725b7cd8c4115c145ae
Sagar Ghimire <info@sagarg.com.np>
2025-02-05 19:33:38 +0545

    cleanup:  plugin code cleanup

--- ckanext/rvr/plugin.py
+++ ckanext/rvr/plugin.py
@@ -1,21 +1,8 @@
-import cgi
-import urllib
-import logging
-from ckan.logic import schema as ckan_schema
-from ckanext.rvr.helpers import is_valid_spatial
-
-from ckanext.rvr.views.dataset import dataset_blueprint
-from ckanext.rvr.views.organization import organization_blueprint
 import logging
 import ckan.plugins.toolkit as tk
 import ckan.plugins as p
 from ckan.lib.plugins import DefaultTranslation
-from ckanext.rvr import actions as rvrActions
-from ckanext.rvr.commands import rvr_spatial
-from ckanext.rvr import helpers as rvrHelpers
-import ckanext.rvr.views as rvrViews
-from ckanext.rvr import validators
-
+from ckanext.rvr import helpers, validators, actions, views
 
 log = logging.getLogger(__name__)
 
@@ -32,10 +19,6 @@ class RvrPlugin(p.SingletonPlugin, DefaultTranslation):
     p.implements(p.IActions)
     p.implements(p.IValidators)
 
-    # IBlueprint
-    def get_blueprint(self):
-        return [dataset_blueprint]
-
     # IConfigurer
     def update_config(self, config_):
         tk.add_template_directory(config_, "templates")
@@ -48,19 +31,19 @@ class RvrPlugin(p.SingletonPlugin, DefaultTranslation):
         Register template helper functions.
         """
         return {
-            "get_latest_created_datasets": rvrHelpers.get_latest_created_datasets,
-            "build_nav_main": rvrHelpers.build_pages_nav_main,
-            "get_specific_page": rvrHelpers.get_specific_page,
-            "get_faq_page": rvrHelpers.get_faq_page,
-            "get_facet_description": rvrHelpers.get_facet_description,
-            "get_cookie_control_config": rvrHelpers.get_cookie_control_config,
-            "is_valid_spatial": is_valid_spatial,
+            "get_latest_created_datasets": helpers.get_latest_created_datasets,
+            "build_nav_main": helpers.build_pages_nav_main,
+            "get_specific_page": helpers.get_specific_page,
+            "get_faq_page": helpers.get_faq_page,
+            "get_facet_description": helpers.get_facet_description,
+            "get_cookie_control_config": helpers.get_cookie_control_config,
+            "is_valid_spatial": helpers.is_valid_spatial,
         }
 
     # IBlueprint
     def get_blueprint(self):
         """Return a Flask blueprint to be registered in the app."""
-        return rvrViews.get_rvr_blueprint()
+        return views.get_rvr_blueprint()
 
     # IFacets
     def dataset_facets(self, facets_dict, package_type):
@@ -77,10 +60,10 @@ class RvrPlugin(p.SingletonPlugin, DefaultTranslation):
         Available via API /api/action/{action-name}
         """
         return {
-            "package_search": rvrActions.package_search,
-            "package_show": rvrActions.package_show,
-            "package_create": rvrActions.package_create,
-            "package_update": rvrActions.package_update,
+            "package_search": actions.package_search,
+            "package_show": actions.package_show,
+            "package_create": actions.package_create,
+            "package_update": actions.package_update,
         }
 
     # IValidators
